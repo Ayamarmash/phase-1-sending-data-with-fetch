7580e4849a5ddc865dcb50aef8159c68a3ab383f
root <root@DESKTOP-JFEPESN.localdomain>
2022-11-03 11:48:05 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+        }),
+      })
+    .then((res) => {
+        return res.json()
+    })
+    .then((jsonResponse) => {
+        let name = document.createElement('p')
+        let email = document.createElement('p')
+        let id = document.createElement('p')
+        name.textContent = jsonResponse.name
+        email.textContent = jsonResponse.email
+        id.textContent = jsonResponse.id
+        document.getElementById("txt").appendChild(name)
+        document.getElementById("txt").appendChild(email)
+        document.getElementById("txt").appendChild(id)
+    })
+    .catch(function (error) {
+        let err = document.createElement('p')
+        err.textContent = error.message
+        document.getElementById("txt").appendChild(err)
+    })
+}
+submitData("Aya", "ayamarmash@gmail.com")
